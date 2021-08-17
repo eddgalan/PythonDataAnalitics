83b34c4a40883265ea8c3b5733a369625cbf9491
Edson Galan <edd.galan@hotmail.com>
2021-08-17 13:45:33 -0500

    Actualizar registros - Ejemplo sencillo para realizar la actualización de registros

--- SQLAlchemySQLite/main.py
+++ SQLAlchemySQLite/main.py
@@ -16,18 +16,32 @@ def run():
     # ..:: Consulta de Datos ::..
     ob = sqlite.session.query(Producto).get(1)
     print("Producto con Id = 1: " + str(ob))
+
     productos = sqlite.session.query(Producto).all()
     print("Todos los productos: " + str(productos))
+
     primer_producto = sqlite.session.query(Producto).first()
     print("Primer resultado del Query: " + str(primer_producto))
+
     num_productos = sqlite.session.query(Producto).count()
     print("Num de Productos: " + str(num_productos))
+
     arroz = sqlite.session.query(Producto).filter_by(nombre='Arroz')
     print("Query : " + str(arroz))
+
     arroz = sqlite.session.query(Producto).filter_by(nombre='Arroz').first()
     print("Producto arroz : " + str(arroz.id))
+
     mayor_1 = sqlite.session.query(Producto).filter(Producto.precio > 1).all()
     print("Productos con precio mayor a 1: " + str(mayor_1))
+
+    # ..:: Actualizar un registro ::..
+    obj1 = sqlite.session.query(Producto).get(1)
+    obj1.nombre = "Pollo Frito"
+    sqlite.session.commit()
+    print("Registro actualizado")
+    productos = sqlite.session.query(Producto).all()
+    print("Todos los productos: " + str(productos))
 
 if __name__ == '__main__':
     sqlite.Base.metadata.drop_all(sqlite.engine) # Límpia la base de datos (La elimina)
