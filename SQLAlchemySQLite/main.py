0a0c6071fe00b71e75717e97ba6211216cd66429
Edson Galan <edd.galan@hotmail.com>
2021-08-17 12:51:24 -0500

    Insertar registros - Registro dos registros a la DB usando el modelo Producto

--- SQLAlchemySQLite/main.py
+++ SQLAlchemySQLite/main.py
@@ -2,8 +2,15 @@ import sqlite
 from models import Producto
 
 def run():
-    pass
+    arroz = Producto('Arroz', 1.25)
+    tacos = Producto('Tacos', 2.50)
+    sqlite.session.add(arroz)
+    sqlite.session.add(tacos)
+    sqlite.session.commit()
+    print("Se agregó un nuevo registro. ID: " + str(arroz.id))
+    print("Se agregó un nuevo registro. ID: " + str(tacos.id))
 
 if __name__ == '__main__':
+    sqlite.Base.metadata.drop_all(sqlite.engine) # Límpia la base de datos (La elimina)
     sqlite.Base.metadata.create_all(sqlite.engine)
     run()
